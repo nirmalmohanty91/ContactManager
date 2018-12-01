76036507247a3dff8823a6ea7dbb6ba5adc93035
Nirmal Mohanty <nirmalmhnt@gmail.com>
2018-12-01 15:25:09 +0530

    state management using context API

---
+++ src/context.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+
+const Context = React.createContext();
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "DELETE_CONTACT":
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          contact => contact.id !== action.payload
+        )
+      };
+
+    default:
+      return state;
+  }
+};
+
+export class Provider extends Component {
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: "Nirmal Mohanty",
+        email: "nirmalmhnt@gmail.com",
+        phone: "9087607304"
+      },
+      {
+        id: 2,
+        name: "Batman",
+        email: "batman@gmail.com",
+        phone: "20937476573"
+      },
+      {
+        id: 3,
+        name: "Robins",
+        email: "robin@gmail.com",
+        phone: "20392857437"
+      }
+    ],
+    dispatch: action => this.setState(state => reducer(state, action))
+  };
+
+  render() {
+    return (
+      <Context.Provider value={this.state}>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+
+export const Consumer = Context.Consumer;
